8e446c823040fd7d29147877d1bdbf10e30b970e
jk kamoru <Crazy.4.JK@gmail.com>
2021-11-18 21:51:45 +0900

    structure set

---
+++ src/test/java/jk/kamoru/flayground/FlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package jk.kamoru.flayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
