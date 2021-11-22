25e4f6e8d35f447706994c54c8755c4cc0943d39
jongkwan-nam <jongkwan.nam@gmail.com>
2021-11-22 17:54:17 +0900

    for u

--- src/main/java/jk/kamoru/flayground/source/FileBasedFlaySource.java
+++ src/main/java/jk/kamoru/flayground/source/FileBasedFlaySource.java
@@ -2,23 +2,25 @@ package jk.kamoru.flayground.source;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
 import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import jk.kamoru.flayground.FlayProperties;
 import jk.kamoru.flayground.commons.FlayUtils;
-import jk.kamoru.flayground.domain.Actress;
 import jk.kamoru.flayground.domain.Flay;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +29,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Repository
 public class FileBasedFlaySource implements FlaySource {
+
+	private static final Pattern FLAYFILE_PATTERN = Pattern.compile("[\\[](.*?)[\\]]");
 
 	@Autowired InfoSourceVideo infoSourceVideo;
 
@@ -63,17 +67,42 @@ public class FileBasedFlaySource implements FlaySource {
 
 			Map<String, Flay> flayMap = instance ? instanceFlayMap : archiveFlayMap;
 			for (File file : listFiles) {
-				Resolved resolved = resolveFile(file);
-				if (resolved.invalid) {
+
+				Matcher matcher = FLAYFILE_PATTERN.matcher(file.getName());
+				String[] part = new String[5];
+				int count = 0;
+				while (matcher.find()) {
+					part[count++] = matcher.group(1).trim();
+				}
+				if (count != 5) {
 					log.warn(" invalid file - {}", file);
 					continue;
 				}
 
-				if (!flayMap.containsKey(resolved.opus)) {
-					flayMap.put(resolved.opus, obtainFlay(resolved, instance));
+				if (!flayMap.containsKey(part[1])) { // compare opus
+					Flay flay = new Flay();
+					flay.setStudio(infoSourceStudio.getOrNew(part[0]));
+					flay.setOpus(part[1]);
+					flay.setTitle(part[2]);
+					flay.setActress(Stream.of(StringUtils.split(part[3], ",")) .map(name -> infoSourceActress.getOrNew(name.trim())).toList());
+					flay.setRelease(part[4]);
+					flay.setVideo(infoSourceVideo.getOrNew(part[1]));
+					flay.setInstance(instance);
+
+					flayMap.put(part[1], flay);
 				}
 
-				addFile(flayMap.get(resolved.opus), file);
+				Flay flay = flayMap.get(part[1]);
+
+				if (FlayUtils.FILE.isVideo(file)) {
+					flay.getFiles().get(Flay.FileType.MOVIE).add(file);
+				} else if (FlayUtils.FILE.isSubtitles(file)) {
+					flay.getFiles().get(Flay.FileType.SUBTITLES).add(file);
+				} else if (FlayUtils.FILE.isImage(file)) {
+					flay.getFiles().get(Flay.FileType.COVER).add(file);
+				} else {
+					log.warn("unknown file {} -> {}", flay.getOpus(), file);
+				}
 			}
 			log.info(String.format("%5s Flay", flayMap.size()));
 		}
@@ -117,62 +146,6 @@ public class FileBasedFlaySource implements FlaySource {
 	@Override
 	public void optimize() {
 		// TODO 배치 처리
-	}
-
-	class Resolved {
-
-		public boolean invalid;
-		public String studio;
-		public String opus;
-		public String title;
-		public String actress;
-		public String release;
-
-	}
-
-	private Resolved resolveFile(File file) {
-		Resolved result = new Resolved();
-		String rowData = file.getName();
-		String[] parts = StringUtils.split(rowData, "]");
-		if (parts == null || parts.length < 5) {
-			result.invalid = true;
-		} else {
-			result.invalid = false;
-			result.studio = StringUtils.replace(parts[0], "[", "");
-			result.opus = StringUtils.replace(parts[1], "[", "");
-			result.title = StringUtils.replace(parts[2], "[", "");
-			result.actress = StringUtils.replace(parts[3], "[", "");
-			result.release = StringUtils.replace(parts[4], "[", "");
-		}
-		return result;
-	}
-
-	private Flay obtainFlay(Resolved resolved, boolean instance) {
-		Flay flay = new Flay();
-		flay.setInstance(instance);
-		flay.setStudio(infoSourceStudio.getOrNew(resolved.studio));
-		flay.setOpus(resolved.opus);
-		flay.setTitle(resolved.title);
-		flay.setActress(findActress(resolved.actress));
-		flay.setRelease(resolved.release);
-		flay.setVideo(infoSourceVideo.getOrNew(resolved.opus));
-		return flay;
-	}
-
-	private List<Actress> findActress(String names) {
-		return Arrays.asList(StringUtils.split(names, ",")).stream().map(name -> infoSourceActress.getOrNew(name)).toList();
-	}
-
-	private void addFile(Flay flay, File file) {
-		if (FlayUtils.FILE.isVideo(file)) {
-			flay.getFiles().get(Flay.FileType.MOVIE).add(file);
-		} else if (FlayUtils.FILE.isSubtitles(file)) {
-			flay.getFiles().get(Flay.FileType.SUBTITLES).add(file);
-		} else if (FlayUtils.FILE.isImage(file)) {
-			flay.getFiles().get(Flay.FileType.COVER).add(file);
-		} else {
-			log.warn("unknown file {} -> {}", flay.getOpus(), file);
-		}
 	}
 
 }
